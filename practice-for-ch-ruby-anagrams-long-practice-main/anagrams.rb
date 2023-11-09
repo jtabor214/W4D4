77a42e80b489c1a0ef5e5691e97d3084b36197cd
Mujahed Ali <mujahed97@gmail.com>
2023-11-09 17:28:27 -0500

    end of day

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -1,4 +1,4 @@
-def first_anagram?(string1, string2)
+def first_anagram?(string1, string2) # O(n!)
     anagrams = string2.chars.permutation.to_a
     anagrams.each do |word|
         return true if word.join("") == string1
@@ -6,5 +6,16 @@ def first_anagram?(string1, string2)
     false   
 end
 
-p first_anagram?("aba", "dog")
-p first_anagram?("aba", "baa")
+def second_anagram?(string1, string2) # O(n^2)
+    string2 = string2.chars # O(n)
+    string1.chars.each_with_index do |char, idx|  # O(n)
+        if string2.find_index(char) != nil  # O(n)
+            target_idx = string2.find_index(char) # O(1)
+            string2.delete_at(target_idx) # O(1)
+        end      
+    end
+    string2.empty? # O(1)
+end 
+
+p second_anagram?("aba", "dog")
+p second_anagram?("aba", "baa")
